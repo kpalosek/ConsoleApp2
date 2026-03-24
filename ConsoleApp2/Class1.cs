d612dcbe53d0263026625f587826164547046c0c
kpalosek <kpalos00@fesb.hr>
2026-03-24 14:42:06 +0100

    Set a to 5

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        int a = 5;
+    }
+}
